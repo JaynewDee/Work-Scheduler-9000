75e833f65f06ec5787fbd1cef571f02f38c862b2
NuminousBlue <jdiehl2236@gmail.com>
2021-09-13 13:53:50 -0500

    Finished code/application fully functional

---
+++ script.js
@@ -0,0 +1,49 @@
+var clockEl = $("#currentDay");
+var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+var saveBtnEl = $(".saveBtn");
+var formEl = $('.container');
+var formArea = document.querySelectorAll('#box');
+
+storageInit();
+renderColor();
+
+function storageInit() {
+     var storedToDos = JSON.parse(localStorage.getItem("toDo"));
+     console.log(Object.values(storedToDos))
+
+     for (var j = 0; j < storedToDos.length; j++) {
+          $(formArea[j]).text(storedToDos[j])
+     }
+     
+}
+
+function displayTime() {
+     var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+     clockEl.text(rightNow);
+}
+
+function renderColor() {
+     for (k = 0; k < formArea.length; k++) {
+          if (moment() > moment('8:59:59 am', 'h:mm:ss a').add(k, 'hour')) {
+          formArea[k].setAttribute('id', 'box-past');
+          console.log(k)
+          } else if (moment().startOf('hour').format('h') == moment().set('hour', 8).add(k, 'hour').format('h')) {
+               formArea[k].setAttribute('id', 'box-present');
+               console.log(k)
+               }
+        
+}};
+
+
+function saveItem(event) {
+     var toDo = [""]
+     event.preventDefault();
+     for (var i = 0; i < formArea.length; i++) {
+          var hourValue = $(formArea[i]).val();
+          toDo.splice(i, 1, hourValue);
+     }
+     localStorage.setItem("toDo", JSON.stringify(toDo));
+}
+
+saveBtnEl.on("click", saveItem);
+setInterval(displayTime, 1000);
